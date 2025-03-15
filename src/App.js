00ad5a6b18dfe1332eab6fcdd99e618daacc478a
Gamddalki <hannahk01@naver.com>
2025-03-15 20:35:14 +0900

    ✨ feat: Add Header, Footer, and Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Footer from "./Components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
