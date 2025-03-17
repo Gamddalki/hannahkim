847394dea7cafe6bbde16ce54b13965f296dc00a
Gamddalki <hannahk01@naver.com>
2025-03-17 23:57:30 +0900

    💄 UI : Adjust hovering effect

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -63,7 +63,7 @@ const TitleOverlay = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.3);
+  background: rgba(0, 0, 0, 0.6);
   padding: 10px;
   display: flex;
   flex-direction: column;
