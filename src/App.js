2104db4e29a619efac174092f4514a74dd318b2e
Gamddalki <hannahk01@naver.com>
2025-03-25 12:46:50 +0900

    💄 UI : Add theme

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Footer from "./Components/Footer";
 import Details from "./Routes/Details";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:category/:id" element={<Details />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:category/:id" element={<Details />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
