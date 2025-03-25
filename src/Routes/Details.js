a132d37b6e5ec5e9570128f5c849ae3e3b273c69
Gamddalki <hannahk01@naver.com>
2025-03-25 12:48:41 +0900

    ✨ feat : Update Details with InfoGrid

--- src/Routes/Details.js
+++ src/Routes/Details.js
@@ -15,20 +15,22 @@ const ContentsWrapper = styled.div`
   width: 60%;
   max-width: 820px;
   h1 {
-    margin-top: 50px;
+    margin: 50px 0;
+    font-size: 50px;
   }
-  h2 {
+  h4 {
     margin-top: 5px;
   }
-  h3 {
+  h5 {
     margin-top: 5px;
-    margin-bottom: 40px;
   }
   span {
     margin: 20px 0;
   }
   a {
-    color: white;
+    font-family: "GmarketSansMedium", -apple-system, sans-serif;
+    line-height: 20px;
+    font-size: 15px;
     color: ${(props) => props.theme.colors.primary};
     margin: 20px 0;
     display: block;
@@ -36,6 +38,29 @@ const ContentsWrapper = styled.div`
     &:hover {
       color: ${(props) => props.theme.colors.hover};
     }
+  }
+`;
+
+const InfoGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin-top: 30px;
+`;
+
+const InfoBlock = styled.div`
+  h5 {
+    margin-bottom: 10px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    color: ${(props) => props.theme.colors.primary};
+    margin-bottom: 7px;
+    line-height: 20px;
+    font-size: 15px;
   }
 `;
 
@@ -88,21 +113,53 @@ const Details = () => {
       </ToHome>
       <ContentsWrapper>
         <h1>{item.title}</h1>
-        <h3>{item.subtitle}</h3>
-        <h4>
+        <h4>{item.subtitle}</h4>
+        <h5>{item.where}</h5>
+        <h5>
           {category === "project"
-            ? `${item.where} (${item.startDate} ~ ${item.endDate})`
-            : `${item.where} | ${item.date}`}
-        </h4>
+            ? `(${item.startDate} ~ ${item.endDate})`
+            : `(${item.date})`}
+        </h5>
         <span>{item.summary}</span>
-        {category === "project" ? (
-          <span>👩🏻‍💻 Tech Stack: {item.techStack}</span>
-        ) : (
-          <span>🏷️ Index Terms: {item.indexTerms}</span>
-        )}
-        <a href={item.link} target="_blank" rel="noopener noreferrer">
-          {category === "project" ? "🔗 Link to Project" : "🔗 Link to Paper"}
-        </a>
+
+        <InfoGrid>
+          <InfoBlock>
+            {category === "project" ? (
+              <>
+                <h5>👩🏻‍💻 Tech Stack</h5>
+                <ul>
+                  {item.techStack.map((tech, index) => (
+                    <li key={index}>{tech}</li>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <>
+                <h5>🏷️ Index Terms</h5>
+                <ul>
+                  {item.indexTerms.map((i, index) => (
+                    <li key={index}>{i}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </InfoBlock>
+          <InfoBlock>
+            <h5>🍓 My Role</h5>
+            <ul>
+              {item.myRole.map((role, index) => (
+                <li key={index}>{role}</li>
+              ))}
+            </ul>
+          </InfoBlock>
+          <InfoBlock>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              {category === "project"
+                ? "🔗 Link to Project"
+                : "🔗 Link to Paper"}
+            </a>
+          </InfoBlock>
+        </InfoGrid>
 
         <ContentBlock>
           {item.content.map((i, index) =>
