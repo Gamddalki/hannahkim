4dc67bcf78f6e2a7660351e33b8024087c943587
Gamddalki <hannahk01@naver.com>
2025-03-31 17:13:14 +0900

    🛠️ build: Fix Dynamic Routing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:category/:id" element={<Details />} />
