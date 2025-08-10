d1fc49531d4a104f9bb377449a446becfd5956d0
Gamddalki <hannahk01@naver.com>
2025-08-11 01:27:46 +0900

    💄 UI : Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,60 +1,37 @@
 import React from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faYoutube,
-  faLinkedinIn,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
 
 const FooterDiv = styled.div`
   width: 100%;
-  min-width: 820px;
   bottom: 0;
   padding: 30px;
   height: 150px;
   background-color: ${(props) => props.theme.colors.footer};
   text-align: center;
-  display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    padding: 20px;
+    height: auto;
+    min-height: 120px;
+  }
+
   span {
     padding: 5px;
     color: ${(props) => props.theme.colors.footerText};
+    display: block;
+    margin: 5px 0;
+    font-size: 0.8rem;
+    line-height: 0.4rem;
   }
-`;
-
-const Icon = styled(FontAwesomeIcon)`
-  color: ${(props) => props.theme.colors.footerText};
-  size: 1x;
-  padding: 5px;
 `;
 
 function Footer() {
   return (
     <FooterDiv>
       <span>&copy; 2025 Hannah Kim</span>
-      <a
-        href="https://www.linkedin.com/in/hannahk01/"
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        <Icon icon={faLinkedinIn} />
-      </a>
-      <a
-        href="https://github.com/Gamddalki/"
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        <Icon icon={faGithub} />
-      </a>
-      <a
-        href="https://www.youtube.com/@dahae01"
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        <Icon icon={faYoutube} />
-      </a>
+      <span>Last Updated: 2025.08</span>
     </FooterDiv>
   );
 }
