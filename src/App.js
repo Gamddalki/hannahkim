670797e4b36751c1467a9abb7b9a500941b98232
Gamddalki <hannahk01@naver.com>
2025-08-11 01:54:50 +0900

    ✨ feat : Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Routes/Home";
+import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Routes/Home";
+import Hannah from "./Routes/Hannah";
+import Projects from "./Routes/Projects";
+import Publications from "./Routes/Publications";
 import Details from "./Routes/Details";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
@@ -10,8 +14,12 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <Router basename={process.env.PUBLIC_URL}>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/hannah" element={<Hannah />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/publications" element={<Publications />} />
             <Route path="/:category/:id" element={<Details />} />
           </Routes>
           <Footer />
