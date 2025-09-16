1be7aed162c6fbd19ee0c6c5433603c7e5b7f179
Gamddalki <hannahk01@naver.com>
2025-09-16 16:56:18 +0900

    💄 UI : Create Publication.js

---
+++ src/Routes/Publications.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import researchData from "../data/researches.json";
+import PageContainer from "../Components/PageContainer";
+import MasonryGrid from "../Components/MasonryGrid";
+
+function Publications() {
+  const navigate = useNavigate();
+
+  const handleCardClick = (publication) => {
+    navigate(`/publications/${publication.id}`);
+  };
+
+  // Helper functions for MasonryGrid
+  const getImageSrc = (publication) =>
+    `${process.env.PUBLIC_URL}/${publication.thumbnail}`;
+  const getTitle = (publication) => publication.title;
+  const getSubtitle = (publication) => publication.subtitle;
+  const getMetaTags = (publication) => publication.indexTerms;
+  const getKey = (publication) => publication.id;
+
+  return (
+    <PageContainer title="PUBLICATIONS">
+      <MasonryGrid
+        items={researchData}
+        onItemClick={handleCardClick}
+        getImageSrc={getImageSrc}
+        getTitle={getTitle}
+        getSubtitle={getSubtitle}
+        getMetaTags={getMetaTags}
+        getKey={getKey}
+        tagField="indexTerms"
+        sortField="date"
+      />
+    </PageContainer>
+  );
+}
+
+export default Publications;
