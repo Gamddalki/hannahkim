20100a709268e9c92731f61a512a170d6b81769e
Gamddalki <hannahk01@naver.com>
2025-10-18 02:33:59 +0900

    ♻️ refactor : optimize ScrolltoTop

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, memo, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 
-const ScrollToTop = () => {
+const ScrollToTop = memo(() => {
   const { pathname } = useLocation();
 
+  const scrollToTop = useCallback(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }, []);
+
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    scrollToTop();
+  }, [pathname, scrollToTop]);
 
   return null;
-};
+});
+
+ScrollToTop.displayName = "ScrollToTop";
 
 export default ScrollToTop;
